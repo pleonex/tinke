7d1c6338505d6cc528d56b90b05e14d9c52f7954
benito356 <benito356@6dce8512-c78e-0f9d-a5fc-7b891d899d37>
2011-04-13 19:35:39 +0000

    * Versión 0.5.0.1 Añadido soporte para mensajes de texto .bmg (MESGbmg1)

---
+++ trunk/Plugins/Nintendo/Nintendo/iBMG.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Nintendo
+{
+    partial class iBMG
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("Codificación");
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "Sección", System.Drawing.SystemColors.WindowText, System.Drawing.SystemColors.Window, new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)))),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "INF1", System.Drawing.SystemColors.WindowText, System.Drawing.SystemColors.Window, new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))))}, -1);
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("# mensajes");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("Tamaño offset");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("Desconocido 1");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("Desconocido 2");
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.listProp = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.numericMsg = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericMsg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(4, 4);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.Size = new System.Drawing.Size(314, 296);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // listProp
+            // 
+            this.listProp.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listProp.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem7,
+            listViewItem8,
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12});
+            this.listProp.Location = new System.Drawing.Point(325, 4);
+            this.listProp.Name = "listProp";
+            this.listProp.Size = new System.Drawing.Size(170, 264);
+            this.listProp.TabIndex = 1;
+            this.listProp.UseCompatibleStateImageBehavior = false;
+            this.listProp.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Campo";
+            this.columnHeader1.Width = 89;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Valor";
+            this.columnHeader2.Width = 75;
+            // 
+            // numericMsg
+            // 
+            this.numericMsg.Location = new System.Drawing.Point(433, 280);
+            this.numericMsg.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericMsg.Name = "numericMsg";
+            this.numericMsg.Size = new System.Drawing.Size(61, 20);
+            this.numericMsg.TabIndex = 2;
+            this.numericMsg.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericMsg.ValueChanged += new System.EventHandler(this.numericMsg_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mostrar mensajes nº:";
+            // 
+            // iBMG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericMsg);
+            this.Controls.Add(this.listProp);
+            this.Controls.Add(this.txtMsg);
+            this.Name = "iBMG";
+            this.Size = new System.Drawing.Size(498, 498);
+            ((System.ComponentModel.ISupportInitialize)(this.numericMsg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.ListView listProp;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.NumericUpDown numericMsg;
+        private System.Windows.Forms.Label label1;
+    }
+}
