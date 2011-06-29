bd702e9b85a24c1ef84100d002b949c691cb5c8e
benito356 <benito356@6dce8512-c78e-0f9d-a5fc-7b891d899d37>
2011-06-29 17:20:42 +0000

    Lectura de archivos SDAT casi completa (falta asignar nombres e IDs verdaderos) Añadida interfaz para mostrar los archivos del SDAT

--- trunk/Plugins/SDAT/SDAT/Properties/AssemblyInfo.cs
+++ trunk/Plugins/SDAT/SDAT/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tinke")]
-[assembly: AssemblyCopyright("Copyright © pleoNeX 2011")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,5 +53,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
--- trunk/Plugins/SDAT/SDAT/iSDAT.Designer.cs
+++ trunk/Plugins/SDAT/SDAT/iSDAT.Designer.cs
@@ -49,10 +49,104 @@ namespace SDAT
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iSDAT));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("ID");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Formato");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Offset");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Tamaño");
+            this.treeFiles = new System.Windows.Forms.TreeView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listProp = new System.Windows.Forms.ListView();
+            this.columnCampo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnValor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeFiles
+            // 
+            this.treeFiles.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeFiles.ImageIndex = 2;
+            this.treeFiles.ImageList = this.imageList;
+            this.treeFiles.Location = new System.Drawing.Point(0, 0);
+            this.treeFiles.Name = "treeFiles";
+            this.treeFiles.SelectedImageIndex = 0;
+            this.treeFiles.Size = new System.Drawing.Size(245, 510);
+            this.treeFiles.TabIndex = 0;
+            this.treeFiles.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeFiles_AfterSelect);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "folder.png");
+            this.imageList.Images.SetKeyName(1, "music.png");
+            this.imageList.Images.SetKeyName(2, "sound.png");
+            this.imageList.Images.SetKeyName(3, "package_go.png");
+            // 
+            // listProp
+            // 
+            this.listProp.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnCampo,
+            this.columnValor});
+            this.listProp.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listProp.Location = new System.Drawing.Point(251, 3);
+            this.listProp.Name = "listProp";
+            this.listProp.Size = new System.Drawing.Size(256, 104);
+            this.listProp.TabIndex = 1;
+            this.listProp.UseCompatibleStateImageBehavior = false;
+            this.listProp.View = System.Windows.Forms.View.Details;
+            // 
+            // columnCampo
+            // 
+            this.columnCampo.Text = "Campo";
+            this.columnCampo.Width = 118;
+            // 
+            // columnValor
+            // 
+            this.columnValor.Text = "Valor";
+            this.columnValor.Width = 121;
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Enabled = false;
+            this.btnExtract.ImageIndex = 3;
+            this.btnExtract.ImageList = this.imageList;
+            this.btnExtract.Location = new System.Drawing.Point(251, 472);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(82, 35);
+            this.btnExtract.TabIndex = 2;
+            this.btnExtract.Text = "Extraer";
+            this.btnExtract.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // iSDAT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.listProp);
+            this.Controls.Add(this.treeFiles);
+            this.Name = "iSDAT";
+            this.Size = new System.Drawing.Size(510, 510);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeFiles;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listProp;
+        private System.Windows.Forms.ColumnHeader columnCampo;
+        private System.Windows.Forms.ColumnHeader columnValor;
+        private System.Windows.Forms.Button btnExtract;
     }
 }
