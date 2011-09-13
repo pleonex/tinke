1964e1e0d3d1dcc9471cbabbc44b3bc81026e78f
benito356 <benito356@6dce8512-c78e-0f9d-a5fc-7b891d899d37>
2011-09-13 23:01:52 +0000

    * Importa más de un archivo a la vez según su nombre

---
+++ trunk/Tinke/Dialog/SelectFile.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Tinke.Dialog
+{
+    partial class SelectFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectFile));
+            this.listFiles = new System.Windows.Forms.ListBox();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listFiles
+            // 
+            this.listFiles.FormattingEnabled = true;
+            this.listFiles.Location = new System.Drawing.Point(13, 39);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(308, 186);
+            this.listFiles.TabIndex = 0;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnAccept.Image = global::Tinke.Properties.Resources.accept;
+            this.btnAccept.Location = new System.Drawing.Point(238, 236);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(83, 28);
+            this.btnAccept.TabIndex = 1;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "There are more than one file with the same name,\r\nselect the correct file";
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSkip.Image = global::Tinke.Properties.Resources.cancel;
+            this.btnSkip.Location = new System.Drawing.Point(149, 236);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(83, 28);
+            this.btnSkip.TabIndex = 3;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSkip.UseVisualStyleBackColor = true;
+            // 
+            // SelectFile
+            // 
+            this.AcceptButton = this.btnAccept;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.CancelButton = this.btnSkip;
+            this.ClientSize = new System.Drawing.Size(333, 276);
+            this.Controls.Add(this.btnSkip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.listFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectFile";
+            this.ShowInTaskbar = false;
+            this.Text = "Choose a file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listFiles;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSkip;
+    }
+}
---
+++ trunk/Tinke/Dialog/SelectFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PluginInterface;
+
+namespace Tinke.Dialog
+{
+    public partial class SelectFile : Form
+    {
+       Archivo[] files;
+
+        public SelectFile()
+        {
+            InitializeComponent();
+        }
+        public SelectFile(Archivo[] files)
+        {
+            InitializeComponent();
+
+            this.files = files;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                String text = "0x" + files[i].id.ToString("x") + " - ";
+                text += (String)files[i].tag + '/' + files[i].name;
+                listFiles.Items.Add(text);
+            }
+        }
+
+        public Archivo SelectedFile
+        {
+            get { return files[listFiles.SelectedIndex]; }
+        }
+    }
+}
