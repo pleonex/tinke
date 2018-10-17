b9374ce80c1474b30ab826c4d2c650876f0a7f28
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-10-17 18:38:53 +0200

    Added DSDecmp library and fixed credits name

--- Plugins/Pack/Pack/ALAR.cs
+++ Plugins/Pack/Pack/ALAR.cs
@@ -4,21 +4,21 @@
 // Copyright (C) 2012
 //
 //   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by 
+//   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
 //
-//   This program is distributed in the hope that it will be useful, 
+//   This program is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details. 
+//   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // </copyright>
 
-// <author>pleoNeX, Daviex94,</author>
+// <author>pleoNeX, Daviex94, Priverop</author>
 // <email>benito356@gmail.com</email>
 // <date>06/07/2012 1:03:49</date>
 // -----------------------------------------------------------------------
