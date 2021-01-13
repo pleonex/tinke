1b427922f93581dc7e016b1081e3884ba92b6981
KerJoe <40915672+KerJoe@users.noreply.github.com>
2021-01-13 23:55:47 +0400

    LAYTON: Support for both London holiday and Main game

--- Plugins/LAYTON/LAYTON/Main.cs
+++ Plugins/LAYTON/LAYTON/Main.cs
@@ -195,9 +195,9 @@ namespace LAYTON
                     }
                     break;
                 case "YLTJ":
-                    if (file.id >= 0x3CA && file.id <= 0x47F)
+                    if ((file.id >= 0x3CA && file.id <= 0x47F) || (file.id >= 0x740 && file.id <= 0xB14))
                         return new Ani(pluginHost, gameCode, file.path).Show_Info(file.id);
-                    else if (file.id >= 0x480 && file.id <= 0x523)
+                    else if ((file.id >= 0x480 && file.id <= 0x523) || (file.id >= 0xB15 && file.id <= 0xF3D))
                     {
                         Bg bg = new Bg(pluginHost, file.path, file.id, file.name);
                         return bg.Get_Control();
